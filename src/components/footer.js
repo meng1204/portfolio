27e1283674a28c7eb8fba0faf2452270512a9925
Nico Chen <meng8859@gmail.com>
2021-03-14 18:15:20 -0700

    Update: lang-zh para

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import personalData from "../personal-data"
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation()
   const data = personalData()
   return (
     <div className="section" id="contact">
       <div className="container">
         <div className="footer-container">
           <Fade bottom cascade>
-            <h1>Contact</h1>
+            <h1>{t('home.contact.name')}</h1>
             <h2>{data.contactSubHeading}</h2>
           </Fade>
           <a className="email-link" href={`mailto:${data.contactEmail}`}>
